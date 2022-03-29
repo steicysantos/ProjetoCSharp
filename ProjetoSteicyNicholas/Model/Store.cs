a62edf86397ee472ede1e337f84cdc9ff69113d9
Steicy <steicymsantos@gmail.com>
2022-03-29 14:29:01 -0300

    atualizando

--- ProjetoSteicyNicholas/Model/Store.cs
+++ ProjetoSteicyNicholas/Model/Store.cs
@@ -6,4 +6,10 @@ public class Store
     private Owner owner;
     private Purchase purchase;
 
+    List<Purchase> purchases = new List<Purchase>();
+
+    public void addNewPurchase(Purchase purchase){
+        purchases.Add(purchase);
+    }
+
 }
