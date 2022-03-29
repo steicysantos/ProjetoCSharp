69f2c85387b1aeead502b366f3ccb8f7ab38fa9f
Steicy <steicymsantos@gmail.com>
2022-03-29 14:37:41 -0300

    atualizando

--- ProjetoSteicyNicholas/Model/Owner.cs
+++ ProjetoSteicyNicholas/Model/Owner.cs
@@ -1,4 +1,17 @@
 ﻿namespace Model;
 
-public class Owner : Person{}
+public class Owner : Person{
+
+    private Owner instance;
+    private Adress adress;
+    public static Owner getInstance(){
+        if(instance == null){
+            instance = new Owner();
+            adress = new Adress();
+        }
+
+        return instance;
+
+    }
+}
 
