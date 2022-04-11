006157caa1bc3312273234bc963b60f238365348
Nicholas <nicholasstelmach@icloud.com>
2022-04-11 14:01:55 -0300

    Adicionando DAO

---
+++ ProjetoSteicyNicholas/DAO/DaoContext.cs
@@ -0,0 +1,25 @@
+﻿namespace DAO;
+
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+
+public class DaoContext
+{
+    public DbSet<Address> address { get; set; }
+    public DbSet<Client> clients {get; set;}
+    public DbSet<Owner> owners { get; set; }
+    public DbSet<Product> products { get; set; }
+    public DbSet<Purchase> purchases { get; set; }
+    public DbSet<Stocks> stocks { get; set; }
+    public DbSet<Store> stores {get; set;}
+    public DbSet<WishList> wishLists {get;set;}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test");
+    }
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
