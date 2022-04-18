39b3acbc015258d645e67326c2d2bbdbc6c5de34
Steicy <steicymsantos@gmail.com>
2022-04-18 16:01:55 -0300

    atualizando

--- ProjetoSteicyNicholas/Model/WishList.cs
+++ ProjetoSteicyNicholas/Model/WishList.cs
@@ -1,23 +1,112 @@
+using System;
+using Interfaces; 
+using DAO;
+using DTO;
+using System.Collections.Generic;
+
 namespace Model;
 
-public class WishList{
+public class WishList : IValidateDataObject, IDataController<WishListDTO, WishList>
+{
     private Client cliente;
     List <Product> listaProdutos=new List<Product>();
+
+    public List<WishListDTO> wishListDTO = new List<WishListDTO>();
 
     public void addProductToWishList(Product product){
         listaProdutos.Add(product);
     }
 
-    public WishList(Client cliente){
-        this.cliente=cliente;
+    public WishList(Client client,List<Product> listaProdutos){
+        this.client =  client;
+
+        this.listaProdutos = listaProdutos;
     }
-    public Client getClient(){
-        return cliente;
+
+    public static WishList convertDTOToModel(WishListDTO obj)
+    {
+        return new WishList(obj.client, obj.listaProdutos);
     }
-    public List<Product> getProducts(){
-        return listaProdutos;
+
+
+    public Boolean validateObject()
+    {
+        return true;
     }
-    public void setClient(Client cliente){
-        this.cliente=cliente;
+
+    public void delete(WishListDTO obj)
+    {
+
     }
+
+    public int save()
+    {
+        var id = 0;
+
+        using(var context = new DAOContext())
+        {
+            var wishList = new DAO.WishList{
+                client = this.client,
+                listaProdutos = this.listaProdutos
+            };
+
+            context.WishList.Add(wishList);
+
+            context.SaveChanges();
+
+            id = wishList.id;
+
+        }
+         return id;
+    }
+
+    public void update(WishListDTO obj)
+    {
+
+    }
+
+    public WishListDTO findById(int id)
+    {
+
+        return new WishListDTO();
+    }
+
+    public List<WishListDTO> getAll()
+    {        
+        return this.wishListDTO;      
+    }
+
+   
+    public WishListDTO convertModelToDTO()
+    {
+        var wishListDTO = new WishListDTO();
+
+        wishListDTO.client = this.client;
+
+        wishListDTO.listaProdutos = this.listaProdutos;
+
+        return wishListDTO;
+    }
+
+    public void setClient(String client)
+    {
+        this.client = client;
+    }
+
+    public void setListaProdutos(List<Product> listaProdutos)
+    {
+        this.listaProdutos = listaProdutos;
+    }
+
+
+    public String getClient()
+    {
+        return this.client;
+    }
+
+    public List<Product> getListaProdutos()
+    {        
+        return this.listaProdutos;
+    }
+
 }
