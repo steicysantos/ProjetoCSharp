26a016f2a451319c88ba0949b1a46a7fb9f97a29
Steicy <steicymsantos@gmail.com>
2022-04-18 13:38:44 -0300

    atualizando

---
+++ ProjetoSteicyNicholas/Interfaces/IDataController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Interfaces;
+public interface IDataController<T, O>
+{
+    public T findById(int id);
+
+    public List<T> getAll();
+
+    public int save();
+
+    public void update(T obj);
+
+    public void delete(T obj);
+
+    public T convertModelToDTO();
+}
