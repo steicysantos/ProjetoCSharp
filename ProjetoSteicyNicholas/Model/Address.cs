8eabdc72689f494d14fcedf1f0a8995b753ee91d
Steicy <steicymsantos@gmail.com>
2022-04-18 15:00:24 -0300

    atualizando

--- ProjetoSteicyNicholas/Model/Address.cs
+++ ProjetoSteicyNicholas/Model/Address.cs
@@ -1,86 +1,159 @@
 using System;
+using Interfaces; 
+using DAO;
+using DTO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Interfaces;
 
-namespace Model
+namespace Model;
+
+public class Address : IValidateDataObject, IDataController<AddressDTO, Address>
 {
+    private String street;
 
-    public class Address : IValidateDataObject<Address>
-    {
-        protected String street = "";
-        protected String city = "";
-        protected String state = "";
-        protected String country = "";
-        protected String posteCode = "";
+    private String city;
 
-        public Address(String street, String city, String state, String country, String posteCode)
-        {
-            this.street = street;
-            this.city = city;
-            this.state = state;
-            this.country = country;
-            this.posteCode = posteCode;
-        }
+    private String state;
 
-        public string getStreet()
-        {
-            return street;
-        }
-        public string getCity()
-        {
-            return city;
-        }
-        public string getState()
-        {
-            return state;
-        }
-        public string getCountry()
-        {
-            return country;
-        }
-        public string getPostalCode()
-        {
-            return posteCode;
-        }
+    private String country;
 
-        public void setStreet(string street)
-        {
-            this.street = street;
-        }
-        public void setCity(string city)
-        {
-            this.city = city;
-        }
-        public void setState(string state)
-        {
-            this.state = state;
-        }
-        public void setCountry(string country)
-        {
-            this.country = country;
-        }
-        public void setPosteCode(string posteCode)
-        {
-            this.posteCode = posteCode;
-        }
+    private String postal_code;
 
-        public Boolean validateObject(Address obj)
-        {
-            if (obj.city == null)
-                return false;
-            if (obj.state == null)
-                return false;
-            if (obj.country == null)
-                return false;
-            if (obj.posteCode == null)
-                return false;
-            if (obj.street == null)
-                return false;
-            return true;
+    public List<AddressDTO> addressDTO = new List<AddressDTO>();
 
-        }
+    public Address(String street,String city, String state,String country , String postal_code){
+        this.street =  street;
+
+        this.city = city;
+
+        this.state = state;
+
+        this.country = country;
+
+        this.postal_code = postal_code;
     }
+
+    public static Address convertDTOToModel(AddressDTO obj)
+    {
+        return new Address(obj.street, obj.city, obj.state, obj.country, obj.postal_code);
+    }
+
+
+    public Boolean validateObject()
+    {
+        return true;
+    }
+
+    public void delete(AddressDTO obj)
+    {
+
+    }
+
+    public int save()
+    {
+        var id = 0;
+
+        using(var context = new DAOContext())
+        {
+            var address = new DAO.Address{
+                street = this.street,
+                city = this.city,
+                state = this.state,
+                country = this.country,
+                postal_code = this.postal_code
+            };
+
+            context.Address.Add(address);
+
+            context.SaveChanges();
+
+            id = address.id;
+
+        }
+         return id;
+    }
+
+    public void update(AddressDTO obj)
+    {
+
+    }
+
+    public AddressDTO findById(int id)
+    {
+
+        return new AddressDTO();
+    }
+
+    public List<AddressDTO> getAll()
+    {        
+        return this.addressDTO;      
+    }
+
+   
+    public AddressDTO convertModelToDTO()
+    {
+        var addressDTO = new AddressDTO();
+
+        addressDTO.street = this.street;
+
+        addressDTO.state = this.state;
+
+        addressDTO.city = this.city;
+
+        addressDTO.country = this.country;
+
+        addressDTO.postal_code = this.postal_code;
+
+        return addressDTO;
+    }
+
+    public void setStreet(String street)
+    {
+        this.street = street;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public void setState(String state)
+    {
+        this.state = state;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public void setPostalCode(String postal_code)
+    {
+        this.postal_code = postal_code;
+    }
+
+    public String getStreet()
+    {
+        return this.street;
+    }
+
+    public String getCity()
+    {        
+        return this.city;
+    }
+
+    public String getState()
+    {
+        return this.state;
+    }
+
+    public String getCountry()
+    {
+        return this.country;
+    }
+
+    public String getPostalCode()
+    {
+        return this.postal_code;
+    }
+
 }
