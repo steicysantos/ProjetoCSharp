d76bef4486168f76c5c4333811503afe4ee982a8
Steicy <steicymsantos@gmail.com>
2022-04-19 14:34:15 -0300

    arrumando erros

--- ProjetoSteicyNicholas/DTO/PurchaseDTO.cs
+++ ProjetoSteicyNicholas/DTO/PurchaseDTO.cs
@@ -1,10 +1,10 @@
 namespace DTO;
 public class PurchaseDTO{
     public DateTime datePurchase;
-    public string purchase_status;
-    public int payment_type;
-    public String numberConfirmation = "";
-    public String numberNF = "";
+    public string purchaseStatus;
+    public int paymentType;
+    public String numberConfirmation;
+    public String numberNF;
     public List<ProductDTO> product = new List<ProductDTO>();
     public List<StoreDTO> store = new List<StoreDTO>();
     public ClientDTO client;
