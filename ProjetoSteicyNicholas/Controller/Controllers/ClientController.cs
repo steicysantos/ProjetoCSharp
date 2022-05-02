156dc4dd2ed77e1327187d6f5d994d2dfa9688dd
Nicholas <nicholasstelmach@icloud.com>
2022-05-02 14:23:53 -0300

    Adiocinando owner e client controller

--- ProjetoSteicyNicholas/Controller/Controllers/ClientController.cs
+++ ProjetoSteicyNicholas/Controller/Controllers/ClientController.cs
@@ -1,20 +1,33 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Model;
-// using DTO;
-// namespace Controller.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using DTO;
+namespace Controller.Controllers;
 
 
-// [ApiController]
-// [Route("[controller]")]
-// public class ClientController : ControllerBase
-// {
-//     [HttpPost(Name = "registerClient")]
-//     public Client registerClient(clientDTO client)
-//     {
-
-//     }
-//     [HttpGet(Name = "getInformations")]
-//     public void getInformations()
-//     {
-//     }
-// }
+[ApiController]
+[Route("client")]
+public class ClientController : ControllerBase
+{
+    [HttpPost]
+    [Route("register")]
+    public object registerClient([FromBody] ClientDTO client)
+    {
+        var cliente=Model.Client.convertDTOToModel(client);
+        var id=cliente.save();
+        return new{
+            name=client.name,
+            date_of_birth=client.date_of_birth,
+            document=client.document,
+            email=client.email,
+            phone=client.name,
+            login=client.login,
+            passwd=client.passwd,
+            address=client.address,
+            id=id
+        };
+    }
+    [HttpGet(Name = "getInformations")]
+    public void getInformations()
+    {
+    }
+}
--- ProjetoSteicyNicholas/Controller/Controllers/OwnerController.cs
+++ ProjetoSteicyNicholas/Controller/Controllers/OwnerController.cs
@@ -1,20 +1,33 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Model;
-// using DTO;
-// namespace Controller.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using DTO;
+namespace Controller.Controllers;
 
 
-// [ApiController]
-// [Route("[controller]")]
-// public class OwnerController : ControllerBase
-// {
-//     [HttpPost(Name = "registerOwner")]
-//     public Owner registerOwner(OwnerDTO owner)
-//     {
-
-//     }
-//     [HttpGet(Name = "getInformations")]
-//     public void getInformations()
-//     {
-//     }
-// }
+[ApiController]
+[Route("owner")]
+public class OwnerController : ControllerBase
+{
+    [HttpPost]
+    [Route("register")]
+    public object registerOwner(OwnerDTO owner)
+    {
+        var ownerr=Model.Owner.convertDTOToModel(owner);
+        var id=ownerr.save();
+        return new{
+            name=owner.name,
+            date_of_birth=owner.date_of_birth,
+            document=owner.document,
+            email=owner.email,
+            phone=owner.name,
+            login=owner.login,
+            passwd=owner.passwd,
+            address=owner.address,
+            id=id
+        };
+    }
+    // [HttpGet(Name = "getInformations")]
+    // public void getInformations()
+    // {
+    // }
+}
