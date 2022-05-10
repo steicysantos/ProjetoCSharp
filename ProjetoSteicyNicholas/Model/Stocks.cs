9176bc28dfc5262a4330393cab060c4391576987
Steicy <steicymsantos@gmail.com>
2022-05-10 13:18:38 -0300

    arrumando stock controller

--- ProjetoSteicyNicholas/Model/Stocks.cs
+++ ProjetoSteicyNicholas/Model/Stocks.cs
@@ -169,44 +169,42 @@ using DAO;
 using System.Collections.Generic;
 using Interfaces;
 using Microsoft.EntityFrameworkCore;
+
 public class Stocks : IValidateDataObject, IDataController<StocksDTO, Stocks>
 {
     private int quantity;
-
     private Double unit_price;
-
     private Store store;
-
     private Product product;
 
-    public List<StocksDTO> stocksDTO = new List<StocksDTO>();
-}
+    // public List<StocksDTO> stocksDTO = new List<StocksDTO>();
 
-public Stocks(Store store){
-    this.store = store;
-}
-public Stocks(Product product){
-    this.product = product;
-}
 
-private Store(){}
+// public Stocks(Store store){
+//     this.store = store;
+// }
+// public Stocks(Product product){
+//     this.product = product;
+// }
 
+
+// GET SET
 public int getQuantity()
 {
     return quantity;
 }
 
-public getUnit_price()
+public Double getUnit_price()
 {
     return unit_price;
 }
 
-public getStore()
+public Store getStore()
 {
     return store;
 }
 
-public getProduct()
+public Product getProduct()
 {
     return product;
 }
@@ -216,7 +214,7 @@ public void setQuantity(int quantity)
     this.quantity = quantity;
 }
 
-public void setUnit_price(double unit_price)
+public void setUnit_price(Double unit_price)
 {
     this.unit_price = unit_price;
 }
@@ -231,13 +229,22 @@ public void setProduct(Product product)
     this.product = product;
 }
 
+
 public bool validateObject()
 {
-    if(this.getQuantity() == null)
+    if(this.quantity == 0)
     {
         return false;
     }
-    if(this.getUnit_price() == null)
+    if(this.unit_price == 0)
+    {
+        return false;
+    }
+    if(this.product == null)
+    {
+        return false;
+    }
+    if(this.store == null)
     {
         return false;
     }
@@ -248,88 +255,108 @@ public bool validateObject()
 
 //DTO
 
-public void delete(StockDTO obj)
-{
-
+public StockDTO convertModelToDTO(){
+    return new StockDTO
+    {
+        quantity = this.quantity,
+        unit_price = this.unit_price,
+        store = this.store.convertModelToDTO(),
+        product = this.product.convertModelToDTO()
+    };
 }
 
-public interface save()
-{
-    var id = 0;
-    using(var context = new DaoContext())
+public static Stock convertDTOToModel(StockDTO stocks){
+    return new Stock
     {
-        var storeDAO = context.stores.Where(char => c.id == store).Single();
-        var productDAO = product.products.Where(char => c.id == product).Single();
-        var stock = new DAO.Stock{
-        store = storeDAO,
-        product = productDAO,
-        quantity = this.quantity,
-        unit_price = this.unit_price
+        quantity = stocks.quantity,
+        unit_price = stocks.unit_price,
+        product = Product.convertDTOToModel(stocks.product),
+        store = Store.convertDTOToModel(stocks.store)
     };
-    context.stocks.Add(stock);
-    context.Entry(stock.store).State = Microsoft.EntityFrameworkCore.EntityState.Unchanged;
-    context.Entry(stock.product).State = Microsoft.EntityFrameworkCore.EntityState.Unchanged;
-    context.SaveChanges();
-    id = stock.id;
+}
+
+ public static Stock convertDAOToModel(DAO.Stock stocks)
+    {
+        return new Stock
+        {
+            quantity = (int)stocks.quantity,
+            unit_price = stocks.unit_price,
+            product = Product.convertDAOToModel(stocks.product),
+            store = Store.convertDAOToModel(stocks.store)
+        };
     }
+////parei aqui
+
+public int save(int storeId, int productId, int quantity, double unit_price)
+{
+    int id;
+    using (var context = new DAOContext())
+    {
+        var storeDAO = context.Store.FirstOrDefault(s => s.id == storeId);
+        var productDAO = context.Product.FirstOrDefault(p => p.id == productId);
+
+        if(storeDAO == null || productDAO == null)
+        {
+            return -1;
+        }
+        DAO.Stock stocks = new DAO.Stock
+        {
+            quantity = quantity,
+            unit_price = unit_price,
+            product = productDAO,
+            store = storeDAO
+        };
+
+        if(context.Stock.FirstOrDefault( s => s.store == stocks.store && s.product == stocks.product) != null)
+        {
+            return -1;
+        }
+
+        context.Stock.Add(stock);
+        context.Entry(stocks.product).State = Microsoft.EntityFrameworkCore.EntityState.Unchaged;
+        context.Entry(stock.store).State = Microsoft.EntityFrameworkCore.EntityState.Unchanged;
+        context.SaveChanges();
+
+        id = stock.id;
+    }
+
     return id;
 }
 
+
+public void delete()
+{
+    using (var context = new DAOContext())
+        {
+            var thisDAO = this.FindDao();
+            if (thisDAO == null)
+                return;
+            context.Stock.Remove(thisDAO);
+            context.SaveChanges();
+        };
+}
 public void update(StockDTO obj)
 {
 
 }
 
-public StockDTO findByID(int id)
+public StockDTO findById()
 {
-    return new StoreDTO();
+    return new StockDTO();
 }
 
-public static object getStockInfo(string store)
+public List<StockDTO> getAll()
 {
-    using(var context = new DaoContext()){
-//      var stockDAO = context.stocks.Include(s => s.)
-        List<object> estoques = new List<object>();
-        foreach(var stock in stocks){
-            estoques.Add(stock);
-        }
-        return estoques;
-    }
+    List<StockDTO> stocks = new List<StockDTO>();
+    return stocks;
 }
 
-public List<StockDTO> getAll(){
-    return this.stockDTO;
+public DAO.Stock? FindDao()
+{
+    using (var context = new DAOContext())
+    {
+        var stock = context.Stock.FirstOrDefault(i => Stock.convertDAOToModel(i) == this);
+        return stock;
+    };
 }
-
-public StockDTO convertModelToDTO(){
-    var stockDTO = new StockDTO();
-    stockDTO.quantity = this.quantity;
-    stockDTO.unit_price = this.unit_price;
-    stockDTO.store = this.store.convertModelToDTO();
-    // foreach(var stocksDTO in this.stocksDTO){
-        
-    // }
-
-    return stockDTO;
-}
-
-public static Stock convertDTOtOmODEL(StockDTO obj){
-    //var stock = new Stock(Store.convertDTOToModel(obj.store));
-
-    var stock = new Stock();
-
-    if(obj.store != null){
-        stock.store = Store.convertDTOToModel(obj.store);
-    }
-
-    if(obj.product != null){
-        stock.product = Product.convertDTOToModel(obj.product);
-    }
-
-    stock.setQuantity(obj.quantity);
-    stock.setUnit_price(obj.unit_price);
-
-    foreach(var purchase in obj.purchases){
-            store.addNewPurchase(Purchase.convertDTOToModel(purchase));
-    }
 }
